d017507af8be51021bcb060bf1230989083588ea
ragnar <zackmwangi998@gmail.com>
2024-01-17 17:10:19 +0300

    if message

--- if.py
+++ if.py
@@ -2,15 +2,22 @@ print("that is a git lesson")
 
 
 print("my next push")
-print ("my next push")
-print ("my next push")
-print ("my next push")
-print ("my next push")
-print ("my next push")
-print ("my next push")
-print ("my next push")
-
-
-
-
-
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
+print("my next push")
