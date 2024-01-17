38cad00dcfdc41da947cac7b0ed271b5272fe7cc
Zac-Mwangi <zackmwangi998@gmail.com>
2024-01-17 17:00:49 +0300

    Update if.py

--- if.py
+++ if.py
@@ -2,5 +2,11 @@ print("that is a git lesson")
 
 
 print ("my next push")
+print ("my next push")
+print ("my next push")
+print ("my next push")
+print ("my next push")
+print ("my next push")
+print ("my next push")
 
 
